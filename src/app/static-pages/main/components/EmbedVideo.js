edd3af6df16284ef49db93ff3fc3741036d17e17
jaya2 <jaya2004kra@gmail.com>
2024-06-01 15:42:53 +0530

    added media and video file

--- src/app/static-pages/main/components/EmbedVideo.js
+++ src/app/static-pages/main/components/EmbedVideo.js
@@ -1,17 +1,40 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import "/src/app/static-pages/main/styles/EmbedVideo.css";
 import ReactPlayer from "react-player";
 
 const EmbedVideo = () => {
+  let videosrc = "/media/Phases Presentation.mp4";
   return (
     <div className="EmbedVideo" id="embed-video">
       <div className="embedvideo-whole-container">
         <div className="embed-video-title">
           Visual Overview: Witness the BlockSecure Process
         </div>
+        <div className="phases-video">
+          <ReactPlayer
+            width="650px"
+            height="550px"
+            url={videosrc}
+            controls={true}
+          />
+          <source src={videosrc} type="video/mp4" />
+        </div>
       </div>
     </div>
   );
 };
 
-export default EmbedVideo;
+const EmbedVideoWrapper = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient ? <EmbedVideo /> : null;
+};
+
+export default EmbedVideoWrapper;
+
