9c204da2aeb45453d20bc59ad3f16d425683cda7
jaya2 <jaya2004kra@gmail.com>
2024-06-01 12:45:09 +0530

    rm

--- src/app/static-pages/main/Main.js
+++ src/app/static-pages/main/Main.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import About from './components/About'
 import Info from './components/Info'
-import Faq from "./components/faq";
 
 const Main = () => {
   return ( 
     <>
     <About />
     <Info />
-    <Faq/>
     </>
   )
 }
