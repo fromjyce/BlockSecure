62ee3426217023ceaf19f1a6c5900ccf95d133db
jaya2 <jaya2004kra@gmail.com>
2024-06-01 12:30:41 +0530

    added main js file

--- src/app/static-pages/main/Main.js
+++ src/app/static-pages/main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from './components/About'
 import Info from './components/Info'
+import Faq from "./components/faq";
 
 const Main = () => {
   return ( 
     <>
     <About />
     <Info />
+    <Faq/>
     </>
   )
 }
